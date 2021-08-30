7f2c8f06f4450c72310758fce43aa494b0e6c9be
bk-demo <78357064+bk-demo@users.noreply.github.com>
2021-08-30 14:10:02 +0200

    (fix): Resource leaks on deserializeFromString

--- terracotta/bootstrap/src/main/java/org/terracotta/quartz/collections/SerializationHelper.java
+++ terracotta/bootstrap/src/main/java/org/terracotta/quartz/collections/SerializationHelper.java
@@ -44,8 +44,9 @@ public class SerializationHelper {
 
   public static Object deserializeFromString(String key) throws IOException, ClassNotFoundException {
     if (key.length() >= 1 && key.charAt(0) == MARKER) {
-      ObjectInputStream ois = new ObjectInputStream(new StringSerializedObjectInputStream(key));
-      return ois.readObject();
+      try(ObjectInputStream ois = new ObjectInputStream(new StringSerializedObjectInputStream(key))) {
+        return ois.readObject();
+      }
     }
 
     return key;
